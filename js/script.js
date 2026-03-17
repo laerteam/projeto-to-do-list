8594597f9ab7f000014f16d3e487c4985ddd530e
LaerteAM <163071979+LaerteAM@users.noreply.github.com>
2026-03-16 21:12:10 -0300

    apertar Enter para adicionar tarefas e mudança leve no javascript
    - adicionada opção de apertar a tecla Enter para adicionar tarefas
    - mudança no javascript respectiva a divisão entre input checkbox e button no elemento "td"

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 let switchOut = document.getElementById('switchOut');
 let switchIn = document.getElementById('switchIn');
-let toggle = document.querySelector('input#switchToggle');
+let switchToggle = document.querySelector('input#switchToggle');
 let lista = document.getElementById('lista');
 
 const switchOnOff = () => {
-    toggle.checked = !toggle.checked;
+    switchToggle.checked = !switchToggle.checked;
     document.body.classList.toggle('escuro');
-    if (toggle.checked) {
+    if (switchToggle.checked) {
         switchIn.style.transform = 'translateX(28px)';
         switchIn.innerHTML = '<i class="fa-solid fa-moon"></i>'
     } else {
         switchIn.style.transform = 'translateX(0)';
-        switchIn.innerHTML = '<p>&#9728;</p>';
+        switchIn.innerHTML = '<i class="fa-solid fa-sun"></i>';
     }
 }
 
@@ -20,25 +20,33 @@ switchOut.addEventListener('click', switchOnOff);
 const adicionar = () => {
     let inputTask = document.querySelector('input#inputTask');
     let task = inputTask.value;
-    inputTask.value = ''
-    inputTask.focus()
+    inputTask.value = '';
+    inputTask.focus();
 
     let tableRow = document.createElement('tr');
     let tableHeader = document.createElement('th');
-    let tableData = document.createElement('td');
+    let tdConcluir = document.createElement('td');
+    let tdEditTh = document.createElement('td');
 
     let p = document.createElement('p');
     p.textContent = task;
     tableHeader.appendChild(p);
-    tableData.innerHTML = '<input class="concluir" type="checkbox" value="Concluir"> <input class="remover" type="button" value="Remover">';
+    tdConcluir.innerHTML = '<input class="concluir" type="checkbox" value="Concluir">';
+    tdEditTh.innerHTML = '<input class="removeTask" type="button" value="Remover"> <input class="editTask" type="button" value="Editar">';
 
     if (!task.trim()) {
         alert('[ERRO] É necessário digitar um valor antes de adiciona-lo.');
     } else {
         lista.appendChild(tableRow);
         tableRow.appendChild(tableHeader);
-        tableRow.appendChild(tableData);
+        tableRow.appendChild(tdConcluir);
+        tableRow.appendChild(tdEditTh);
     }
 }
 
-document.getElementById('addTask').addEventListener('click', adicionar)
+document.getElementById('addTask').addEventListener('click', adicionar);
+document.querySelector('input#inputTask').addEventListener('keydown', function(evento) {
+    if (evento.key === 'Enter') {
+        adicionar();
+    }
+})
